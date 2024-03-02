bcc4cf242d10c2a21ec9884533574a6126a37e71
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-03-02 13:50:20 -0500

    hrhhh

--- ExpenseTracker/src/ExpenseTrackerLauncher.java
+++ ExpenseTracker/src/ExpenseTrackerLauncher.java
@@ -2,6 +2,8 @@
 public class ExpenseTrackerLauncher {
 
 	public static void main(String[] args) {
+		WindowPractice frame = new WindowPractice();
+		frame.setVisible(true);
 		new ExpenseTrackerGUI().setLocationRelativeTo(null);
 	}
 
