6d177b8b9aeaa8c961e9ee141471b2d68bd2f02f
crewsdel <124068095+crewsdel@users.noreply.github.com>
2024-03-02 16:03:58 -0500

    Update CreateUserGUI.java

--- ExpenseTracker/src/CreateUserGUI.java
+++ ExpenseTracker/src/CreateUserGUI.java
@@ -1,5 +1,4 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -7,7 +6,6 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
