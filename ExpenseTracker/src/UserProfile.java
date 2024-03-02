ed975bf37bf45b61938f2201e84ed38bb63ae8a2
Keons2448 <131809579+Keons2448@users.noreply.github.com>
2024-03-02 15:22:25 -0500

    UserProfile class methods push
    added update methods for each of the data values included in userProfile to be used when a user edits their profile.

--- ExpenseTracker/src/UserProfile.java
+++ ExpenseTracker/src/UserProfile.java
@@ -1,28 +1,55 @@
 
 public class UserProfile {
+//	class variables
 	private String FName;
 	private String LName;
 	private String password;
-	private float expences[];
+	private float expenses[];
 	private float revenue[];
 	
-	public UserProfile(String FName,String LName,String password, float expences[],float revenue[]) {
+//	constructor for existing expenses and revenues
+	public UserProfile(String FName,String LName,String password, float expenses[],float revenue[]) {
 		this.FName = FName;
 		this.LName = LName;
 		this.password = password;
-		this.expences = expences;
+		this.expenses = expenses;
 		this.revenue = revenue;
-		
 	}
+//	constructor for a brand new user
 	public UserProfile(String FName,String LName,String password) {
 		this.FName = FName;
 		this.LName = LName;
 		this.password = password;
 	}
+//	getter functions
 	public String getFName() {return FName;}
 	public String getLName() {return LName;}
 	public String getPassword() {return password;}
-	public float[] getExpences() {return expences;}
+	public float[] getExpences() {return expenses;}
 	public float[] getRevenue() {return revenue;}
 	
+//	function to update the first name (FName) of a user
+	public void updateFName(String newName) {
+		this.FName = newName;
+	}
+	
+//	function to update the last name (LName) of a user
+	public void updateLName(String newName) {
+		this.LName = newName;
+	}
+	
+//	function to update the password (password) of a user
+	public void updatePassword(String newPassword) {
+		this.password = newPassword;
+	}
+	
+//	function to update the expenses list (expenses[]) with a updated list
+	public void updateExpences(float[] newExpenses) {
+		this.expenses = newExpenses;
+	}
+	
+//	function to update the revenue list (revenue[]) with a updated list
+	public void updateRevenue(float[] newRevenue) {
+		this.revenue =  newRevenue;
+	}
 }
