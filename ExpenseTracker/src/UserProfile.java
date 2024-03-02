afb1c5aa37a07e75b30f1da122c7ea475cee4acb
Keons2448 <131809579+Keons2448@users.noreply.github.com>
2024-03-02 15:04:03 -0500

    user update
    updated the user class to include a password variable as well as its corresponding functions.

--- ExpenseTracker/src/UserProfile.java
+++ ExpenseTracker/src/UserProfile.java
@@ -2,21 +2,26 @@
 public class UserProfile {
 	private String FName;
 	private String LName;
+	private String password;
 	private float expences[];
 	private float revenue[];
 	
-	public UserProfile(String FName,String LName,float expences[],float revenue[]) {
+	public UserProfile(String FName,String LName,String password, float expences[],float revenue[]) {
 		this.FName = FName;
 		this.LName = LName;
+		this.password = password;
 		this.expences = expences;
 		this.revenue = revenue;
+		
 	}
-	public UserProfile(String FName,String LName) {
+	public UserProfile(String FName,String LName,String password) {
 		this.FName = FName;
 		this.LName = LName;
+		this.password = password;
 	}
 	public String getFName() {return FName;}
 	public String getLName() {return LName;}
+	public String getPassword() {return password;}
 	public float[] getExpences() {return expences;}
 	public float[] getRevenue() {return revenue;}
 	
